6d4ffe6ce6770793242c9cef16f956a1f2df0d4c
Anurag Khanna <khannaa395@gmail.com>
2021-07-19 19:09:47 +0530

    C# scripts

---
+++ Assets/Scipts/LightChangerScript.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+
+public class LightChangerScript : MonoBehaviour {
+
+	public Light My_Light;
+	private Color[] Light_Colors ={new Color (1,0,0),new Color (0,1,0), new Color (0,0,1)};
+	private int i = 0;
+	private AudioSource BeginningSceneSound;
+
+	private void ChangeLightColor()
+	{
+		My_Light.color = Light_Colors [i];
+		i += 1;
+		if (i == 3)
+			i = 0;
+		Invoke("ChangeLightColor",0.5f);
+	}
+
+	void Start()
+	{
+		Invoke("ChangeLightColor",2);
+		BeginningSceneSound = GetComponent <AudioSource> ();
+		BeginningSceneSound.Play ();
+	}
+
+
+}
